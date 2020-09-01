4a9b0e98af90bd6ac09a0b32386818a91f4caafd
maldestor95 <maldestor95@gmail.com>
2020-09-01 09:17:10 +0200

    feat(workout): update equipment list

--- src/lib/equipment.ts
+++ src/lib/equipment.ts
@@ -1,5 +1,15 @@
 export enum Equipment {
  treadmill= "TREADMILL",
  legpress="LEGPRESS",
+ abductor="ABDUCTOR",
+ lowrow="LOW ROW",
+ armcurl="ARM CURL",
+ armextension="ARM EXTENSION",
+ pectoral="PECTORAL",
+ rotarytorso="ROTARY TORSO",
+ chestpress="CHEST PRESS",
+ rowingmachine="ROWING MACHINE",
+ bike="BIKE",
+ ellipticcycle="ELLIPTIC CYCLE",
  none="NONE"
 }
